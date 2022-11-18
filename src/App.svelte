2bef8df0ba09f343e51c5362e4464afb92ac87bb
Nicholas Moen <arcanemachine@tutanota.com>
2022-11-18 01:51:22 -0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import HelloWorld from './lib/HelloWorld.svelte'
+</script>
+
+<main>
+  <div class="card">
+    <HelloWorld />
+  </div>
+
+</main>
+
+<style>
+</style>
---
+++ src/lib/HelloWorld.svelte
@@ -0,0 +1,13 @@
+<script type="ts">
+  import './HelloWorld.ts';
+
+  const user: User = {
+    id: 1,
+    name: "Bob",
+    age: 32,
+  }
+</script>
+
+<button>
+  Hello, {user.id}!
+</button>
